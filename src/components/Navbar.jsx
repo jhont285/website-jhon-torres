639e4e85a18822beb6e376b52cd275ca240853a8
Jhon Torres <jhetorresto@unal.edu.co>
2017-11-11 14:39:51 -0500

    fixed deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,62 +1,55 @@
 import { Link } from 'react-router-dom';
-import React, { Component } from 'react';
+import React from 'react';
 import FontAwesome from 'react-fontawesome';
 
-class Navbar extends Component {
-  render() {
-    return (
+const Navbar = () => (
+  <nav className="navbar navbar-inverse navbar-fixed-top">
+    <div className="container">
+      <div className="navbar-header">
+        <button
+          type="button"
+          className="navbar-toggle collapsed"
+          data-toggle="collapse"
+          data-target="#bs-example-navbar-collapse-2"
+        >
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+        </button>
+        <Link className="navbar-brand" to="/">
+          <span className="glyphicon glyphicon-send" aria-hidden="true" />&nbsp;Jhon Torres
+        </Link>
+      </div>
 
-      <nav className="navbar navbar-inverse navbar-fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <button
-              type="button"
-              className="navbar-toggle collapsed"
-              data-toggle="collapse"
-              data-target="#bs-example-navbar-collapse-2"
-            >
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-              <span className="icon-bar" />
-            </button>
-            <Link className="navbar-brand" to="/">
-              <span className="glyphicon glyphicon-send" aria-hidden="true" />&nbsp;Jhon Torres
+      <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <Link to="/">
+              <FontAwesome name="home" /> Home
             </Link>
-          </div>
+          </li>
+          <li>
+            <Link to="/projects">
+              <FontAwesome name="keyboard-o" /> Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/haveFun">
+              <FontAwesome name="gamepad" /> Have fun
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact">
+              <FontAwesome name="gamepad" /> Contact me
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
 
-          {/* <!-- items --> */}
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
-
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <Link to="/">
-                  <FontAwesome name="home" /> Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/projects">
-                  <FontAwesome name="keyboard-o" /> Projects
-                </Link>
-              </li>
-              <li>
-                <Link to="/haveFun">
-                  <FontAwesome name="gamepad" /> Have fun
-                </Link>
-              </li>
-              <li>
-                <Link to="/contact">
-                  <FontAwesome name="gamepad" /> Contact me
-                </Link>
-              </li>
-            </ul>
-          </div>
-          {/* <!-- .narvar-collapse--> */}
-        </div>
-        {/* <!-- .container --> */}
-      </nav>
-    );
-  }
-}
 
 export default Navbar;
