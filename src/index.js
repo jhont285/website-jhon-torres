b4e7a2a3ad1c9d38ff457c074d93d4e3fb83b5f6
Jhon Torres <jhetorresto@unal.edu.co>
2017-12-05 13:33:30 -0500

    fix error for deploy

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
